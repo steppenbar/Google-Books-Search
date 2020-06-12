c698f2caf6d9e753fc61033509cf6118ac26b10a
Steppenbar <hiwattslo@gmail.com>
2020-06-11 17:09:48 -0700

    added components and API request

---
+++ google/src/Components/Books.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import SearchArea from './SearchArea'
+import request from 'superagent';
+
+
+class Books extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            books:[],
+            searchField:''
+        }
+    }
+searchBook = (e) =>{
+    e.preventDefault();
+    request
+    .get("https://www.googleapis.com/books/v1/volumes?")
+    .query({q: this.searchField})
+    .then((data) => {
+        console.log(data);
+    })
+
+}
+    handleSearch = (e) =>{
+        this.setState({searchField:e.target.value})
+    }
+
+  render(){
+    return(
+      <div>
+          <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch}/>
+
+      </div>
+    );
+  }
+}
+
+export default Books;
---
+++ google/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header>
+            <i className="fas fa-book-dead fa-4x"></i>
+            <h1>Google Books</h1>
+        </header>
+    )
+}
+
+export default Header;
